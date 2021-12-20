311fa675e7aab50fc7a03e4997ce4ed2f064d9a7
AnnaAvl <86964707+AnnaAvl@users.noreply.github.com>
2021-12-20 13:32:50 +0300

    New commit

--- src/com/Main.java
+++ src/com/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My first commit");
         System.out.println("Hello World!");
+        System.out.println("I'm here!");
         System.out.println("Bye!");
     }
 }
