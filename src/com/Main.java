2d66937464aa40b9c0368a4b0ffc8934fdeb3353
buried9866 <buried9866@gmail.com>
2021-12-20 12:56:05 +0300

    4 commit

--- src/com/Main.java
+++ src/com/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My first commit");
         System.out.println("Hello World!");
-        System.out.println("My 3 commit");
+        System.out.println("Bye!");
     }
 }
